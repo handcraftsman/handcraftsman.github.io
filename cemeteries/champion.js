968e0908f5fe71a14ef1560a5929e7a0ab9642da
Clinton Sheppard <gar3ts@gmail.com>
2013-08-25 19:56:47 -0500

    fix image file suffixes

--- cemeteries/champion.js
+++ cemeteries/champion.js
@@ -2,9 +2,9 @@
 'dateTimeForamt': 'iso8601',
 'events': [
 
-  {'start': '11 Dec 1879','title': 'Corelia Josefhene Champion', 'description': 'Name: Corelia Josefhene Champion<BR>Born: 5 Feb 1871<BR>Died: 11 Dec 1879<BR>', 'image': 'champion_0010.png'},
-  {'start': '12 Dec 1900','title': 'John Champion', 'description': 'Name: John Champion<BR>Born: 25 Oct 1817<BR>Died: 12 Dec 1900<BR>', 'image': 'champion_0007.png'},
-  {'start': '19 Oct 1869','title': 'Calvin Whitfield Champion', 'description': 'Name: Calvin Whitfield Champion<BR>Born: 20 Mar 1869<BR>Died: 19 Oct 1869<BR>', 'image': 'champion_0009.png'},
-  {'start': '24 Dec 1868','title': 'Naomi Jane Champion', 'description': 'Name: Naomi Jane Champion<BR>Born: 14 Jul 1834<BR>Died: 24 Dec 1868<BR>', 'image': 'champion_0008.png'}
+  {'start': '11 Dec 1879','title': 'Corelia Josefhene Champion', 'description': 'Name: Corelia Josefhene Champion<BR>Born: 5 Feb 1871<BR>Died: 11 Dec 1879<BR>', 'image': 'champion_0010.jpg'},
+  {'start': '12 Dec 1900','title': 'John Champion', 'description': 'Name: John Champion<BR>Born: 25 Oct 1817<BR>Died: 12 Dec 1900<BR>', 'image': 'champion_0007.jpg'},
+  {'start': '19 Oct 1869','title': 'Calvin Whitfield Champion', 'description': 'Name: Calvin Whitfield Champion<BR>Born: 20 Mar 1869<BR>Died: 19 Oct 1869<BR>', 'image': 'champion_0009.jpg'},
+  {'start': '24 Dec 1868','title': 'Naomi Jane Champion', 'description': 'Name: Naomi Jane Champion<BR>Born: 14 Jul 1834<BR>Died: 24 Dec 1868<BR>', 'image': 'champion_0008.jpg'}
 ]
 }
--- cemeteries/rosary.js
+++ cemeteries/rosary.js
@@ -2,15 +2,15 @@
 'dateTimeForamt': 'iso8601',
 'events': [
 
-  {'start': '14 Apr 2004','title': 'Lawrence Murphy', 'description': 'Name: Lawrence Murphy<BR>Born: 12 May 1936<BR>Died: 14 Apr 2004<BR>Note: s/w Kathleen<BR>', 'image': 'rosary_5245.png'},
-  {'start': '16 Aug 2004','title': 'Cesar Rocha', 'description': 'Name: Cesar Rocha<BR>Born: 5 Mar 1993<BR>Died: 16 Aug 2004<BR>', 'image': 'rosary_5246.png'},
-  {'start': '17 Sep 2004','title': 'Mary Alice', 'description': 'Name: Mary Alice<BR>Born: <BR>Died: 17 Sep 2004<BR>Note: no last name<BR>', 'image': 'rosary_5249.png'},
-  {'start': '19 Oct 2003','title': 'Gabriel Arellano', 'description': 'Name: Gabriel Arellano<BR>Born: <BR>Died: 19 Oct 2003<BR>', 'image': 'rosary_5240.png'},
-  {'start': '31 Dec 2004','title': 'Dwane Newby', 'description': 'Name: Dwane Newby<BR>Born: 1958<BR>Died: 2004<BR>Note: metal funeral marker<BR>', 'image': 'rosary_5248.png'},
-  {'start': '31 Dec 2004','title': 'Amy Michelle Shields', 'description': 'Name: Amy Michelle Shields<BR>Born: 1983<BR>Died: 2004<BR>Note: metal funeral marker<BR>', 'image': 'rosary_5242.png'},
-  {'start': '31 Dec 2005','title': 'Marcus A. Rodriguez', 'description': 'Name: Marcus A. Rodriguez<BR>Born: 2005<BR>Died: 2005<BR>Note: metal funeral marker<BR>', 'image': 'rosary_5241.png'},
-  {'start': '03 Feb 2005','title': 'Howard F Sudduth', 'description': 'Name: Howard F Sudduth<BR>Born: 6 Sep 1925<BR>Died: 3 Feb 2005<BR>Note: s/w Constance<BR>', 'image': 'rosary_5247.png'},
-  {'start': '05 Apr 1998','title': 'Thomas Furlong', 'description': 'Name: Thomas Furlong<BR>Born: <BR>Died: 5 Apr 1998<BR>', 'image': 'rosary_5239.png'},
-  {'start': '08 May 2004','title': 'Marvin Fredrick Rau', 'description': 'Name: Marvin Fredrick Rau III<BR>Born: 29 Jan 1960<BR>Died: 8 May 2004<BR>', 'image': 'rosary_5243.png'}
+  {'start': '14 Apr 2004','title': 'Lawrence Murphy', 'description': 'Name: Lawrence Murphy<BR>Born: 12 May 1936<BR>Died: 14 Apr 2004<BR>Note: s/w Kathleen<BR>', 'image': 'rosary_5245.jpg'},
+  {'start': '16 Aug 2004','title': 'Cesar Rocha', 'description': 'Name: Cesar Rocha<BR>Born: 5 Mar 1993<BR>Died: 16 Aug 2004<BR>', 'image': 'rosary_5246.jpg'},
+  {'start': '17 Sep 2004','title': 'Mary Alice', 'description': 'Name: Mary Alice<BR>Born: <BR>Died: 17 Sep 2004<BR>Note: no last name<BR>', 'image': 'rosary_5249.jpg'},
+  {'start': '19 Oct 2003','title': 'Gabriel Arellano', 'description': 'Name: Gabriel Arellano<BR>Born: <BR>Died: 19 Oct 2003<BR>', 'image': 'rosary_5240.jpg'},
+  {'start': '31 Dec 2004','title': 'Dwane Newby', 'description': 'Name: Dwane Newby<BR>Born: 1958<BR>Died: 2004<BR>Note: metal funeral marker<BR>', 'image': 'rosary_5248.jpg'},
+  {'start': '31 Dec 2004','title': 'Amy Michelle Shields', 'description': 'Name: Amy Michelle Shields<BR>Born: 1983<BR>Died: 2004<BR>Note: metal funeral marker<BR>', 'image': 'rosary_5242.jpg'},
+  {'start': '31 Dec 2005','title': 'Marcus A. Rodriguez', 'description': 'Name: Marcus A. Rodriguez<BR>Born: 2005<BR>Died: 2005<BR>Note: metal funeral marker<BR>', 'image': 'rosary_5241.jpg'},
+  {'start': '03 Feb 2005','title': 'Howard F Sudduth', 'description': 'Name: Howard F Sudduth<BR>Born: 6 Sep 1925<BR>Died: 3 Feb 2005<BR>Note: s/w Constance<BR>', 'image': 'rosary_5247.jpg'},
+  {'start': '05 Apr 1998','title': 'Thomas Furlong', 'description': 'Name: Thomas Furlong<BR>Born: <BR>Died: 5 Apr 1998<BR>', 'image': 'rosary_5239.jpg'},
+  {'start': '08 May 2004','title': 'Marvin Fredrick Rau', 'description': 'Name: Marvin Fredrick Rau III<BR>Born: 29 Jan 1960<BR>Died: 8 May 2004<BR>', 'image': 'rosary_5243.jpg'}
 ]
 }
